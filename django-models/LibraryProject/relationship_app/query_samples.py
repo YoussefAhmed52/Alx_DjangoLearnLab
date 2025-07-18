71081aaf082e27de800144923be068c9cde5bd69
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-18 14:51:25 -0400

    Relation

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,6 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def run_queries():
-    # 1. Query all books by a specific author
     author_name = "J.K. Rowling"
     try:
         author = Author.objects.get(name=author_name)
@@ -12,7 +11,6 @@ def run_queries():
     except Author.DoesNotExist:
         print(f"No author found with name: {author_name}")
 
-    # 2. List all books in a specific library
     library_name = "Central Library"
     try:
         library = Library.objects.get(name=library_name)
@@ -23,9 +21,8 @@ def run_queries():
     except Library.DoesNotExist:
         print(f"No library found with name: {library_name}")
 
-    # 3. Retrieve the librarian for a library
     try:
-        librarian = library.librarian
+        librarian = Librarian.objects.get(library)
         print(f"\n👤 Librarian of {library_name}: {librarian.name}")
     except:
         print("Couldn't find the librarian for that library.")
