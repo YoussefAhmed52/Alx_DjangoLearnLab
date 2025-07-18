2e09ef201e2ce251b9bafa6570c8884e39c6b662
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-18 16:19:32 -0400

    veiw

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Library , Book
+from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()  # get all books from database
