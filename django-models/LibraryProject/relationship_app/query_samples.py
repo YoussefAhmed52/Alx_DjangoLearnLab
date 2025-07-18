833d9f81a176a2645579b48fafd938bedd439b86
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-18 14:49:37 -0400

    Relation

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -16,7 +16,7 @@ def run_queries():
     library_name = "Central Library"
     try:
         library = Library.objects.get(name=library_name)
-        books_in_library = Book.objects.filter(library=library)
+        books_in_library = library.books.all()
         print(f"\n📚 Books in {library_name}:")
         for book in books_in_library:
             print(f"- {book.title}")
