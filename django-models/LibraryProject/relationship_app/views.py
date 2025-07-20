4cc9b92ea1a1dfba96c32d81aecb16c54b59a8f8
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-20 15:15:48 -0400

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,15 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 # Register View
 def register_view(request):
@@ -21,13 +29,13 @@ def register_view(request):
 # Login View
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = UserCreationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('home')
     else:
-        form = AuthenticationForm()
+        form = UserCreationForm()
     return render(request, 'relationship_app/login.html', {'form': form})
 
 # Logout View
