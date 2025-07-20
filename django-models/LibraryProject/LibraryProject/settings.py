f150edad38f89e0bc2ea0083f993af99eb80a9f1
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-20 14:36:32 -0400

    Django

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Library , Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto-login after registration
+            return redirect('home')  # Replace with your home view
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 def list_books(request):
     books = Book.objects.all()  # get all books from database
-    return render(request, 'relationship_app/list_books.html', {'object_list': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
