2ab94a4d8621fbdca1ce4a8c23a838f0e222bf19
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-26 15:08:29 -0400

    advanced

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,24 +10,21 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == 'Librarian'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return user.is_authenticated and user.userprofile.role == 'Member'
-
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'roles/admin_view.html')
 
-
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'roles/librarian_view.html')
-
 
 @user_passes_test(is_member)
 def member_view(request):
