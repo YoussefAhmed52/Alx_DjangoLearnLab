0484cbe6b7ce31309fddc9dbb24c07370057ed61
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-26 15:42:01 -0400

    advanced

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,5 +1,17 @@
-
 from django.contrib import admin
+# views/admin_view.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+@login_required
+def admin_view(request):
+    try:
+        if request.user.userprofile.role != 'Admin':
+            return redirect('login')  # or use a 403 forbidden page
+    except UserProfile.DoesNotExist:
+        return redirect('login')
+
+    return render(request, 'roles/admin_view.html')
+
+# Register your models here.
