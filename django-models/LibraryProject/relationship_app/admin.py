3c0306b33a8702c434f2aae2a213f40a29f8fe5f
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-26 02:32:06 -0400

    templates

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 # views/admin_view.py
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from models import UserProfile
+from .models import UserProfile
 
 @login_required
 def admin_view(request):
