0d275cb46e7518d8f49fd6027ab5efe5e996e2a4
YoussefAhmed52 <yousef3012001@gmail.com>
2025-07-27 16:37:12 -0400

    advanced

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -6,7 +6,7 @@ from .models import Book
 # Only users with 'can_edit' permission can access this view
 @permission_required('library_app.can_edit', raise_exception=True)
 def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+    books = get_object_or_404(Book, id=book_id)
     # Logic to edit the book (form, POST/GET handling)
-    return render(request, 'edit_book.html', {'book_list': book})
+    return render(request, 'edit_book.html', {'book_list': books})
 # Create your views here.
