d727da33af7e4c7e2e63b99168a7b19f15e3889d
YoussefAhmed52 <yousef3012001@gmail.com>
2025-08-03 16:10:13 -0400

    api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import book_list , BookList
+from django.urls import path , include
+from .views import book_list , BookList , BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', book_list),
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -12,9 +12,20 @@ def book_list(request):
     return HttpResponse(output)
 
 from rest_framework.generics import ListAPIView
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+
 class BookList(ListAPIView):
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+    queryset = Book.objects.all().order_by('id')
     serializer_class = BookSerializer
