745ef8c41e24c661333cccb4161aac9712c6ffec
YoussefAhmed52 <yousef3012001@gmail.com>
2025-08-03 16:17:29 -0400

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Book
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
+from rest_framework import generics, viewsets
+
+
 
 def book_list(request):
     books = Book.objects.all()
@@ -11,21 +18,18 @@ def book_list(request):
         output += f"Title: {book.title}, Author: {book.author}<br>"
     return HttpResponse(output)
 
-from rest_framework.generics import ListAPIView
-from rest_framework import generics, viewsets
-from .models import Book
-from .serializers import BookSerializer
-
-
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
-    
+
 class BookViewSet(viewsets.ModelViewSet):
     """
-    A viewset that provides default `create()`, `retrieve()`, `update()`,
-    `partial_update()`, `destroy()` and `list()` actions.
+    A viewset for viewing and editing book instances.
+    Requires token authentication to access.
     """
     queryset = Book.objects.all().order_by('id')
     serializer_class = BookSerializer
+    
+    permission_classes = [IsAuthenticated]
+    
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,8 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken', 
     'api',
 ]
+
+REST_FRAMEWORK = {
+    # Set TokenAuthentication as the default scheme for the entire project.
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
